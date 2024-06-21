a0e5a863460abdf938d18440ddfee33f944a7547
KerolosAtef <keroatef295@gmail.com>
2024-06-21 03:21:46 +0300

    empty_web_branch

--- global_apprerance/tvqa/Run_full_pipeline.sh
+++
@@ -1,7 +0,0 @@
-
-python global_appearance.py 
-python collect_together.py
-python remove_empty_folders.py
-
-## Filter the images humanly and after having the unique images, run the following script
-
--- global_apprerance/tvqa/remove_empty_folders.py
+++
@@ -1,8 +0,0 @@
-import os 
-import json
-
-for season in os.listdir('character_cropped_images_long'):
-    for episode in os.listdir(os.path.join('character_cropped_images_long',season)):
-        for character in os.listdir(os.path.join('character_cropped_images_long',season,episode)):
-            if len (os.listdir(os.path.join('character_cropped_images_long',season,episode,character)))<2:
-                os.system(f"rm -r {os.path.join('character_cropped_images_long',season,episode,character)}")
